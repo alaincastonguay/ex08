5e777e55c99649c594d48bc8be97a3901f6bed71
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-03-26 11:46:54 -0400

    add doc

--- bin/ex08_2.dart
+++ bin/ex08_2.dart
@@ -7,28 +7,33 @@ void main(){
   print("ex08_2"); 
   print("");
   
-lastNameSorting(var members){
-  members.sort((a, b) => a['lastname'].compareTo(b['lastname']));
+  lastNameSorting(var members) {
+    members.sort((a, b) => a['lastname'].compareTo(b['lastname']));
+    
+    print("Liste des membres classe par nom de famille:"); 
+    print(""); 
+    printRow(members); print(''); print('');
+  }
   
-  print("Liste des membres classe par nom de famille:"); 
-  print(""); 
-  printRow(members); print(''); print('');}
   lastNameSorting(members);
   
-  firstNameSorting(var members){
-  members.sort((a, b) => a['firstname'].compareTo(b['firstname']));
-
-  print("Liste des membres classe par prenom:");  
-  print(""); 
-  printRow(members); print(''); print('');}
+  firstNameSorting(var members) {
+    members.sort((a, b) => a['firstname'].compareTo(b['firstname']));
+  
+    print("Liste des membres classe par prenom:");  
+    print(""); 
+    printRow(members); print(''); print('');
+  }
+  
   firstNameSorting(members);
   
-  lastNameStartsR(var members, var letter){
-  
+  lastNameStartsR(var members, var letter) {
     print("Liste noms de famille commencant par la lettre 'C':"); 
     print(""); 
-    for(var i in members){
-      if(i['lastname'][0] == letter)
-        print(i);}}
-  lastNameStartsR(members, 'C');
+    for(var i in members) {
+      if (i['lastname'][0] == letter) print(i);
+    }
   }
+  
+  lastNameStartsR(members, 'C');
+}
